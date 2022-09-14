f02cd44a53aed50729f1c69fdc1253cf6dd43f5c
Emanuel Monzon <emanuel-monzon97@outlook.com>
2022-09-14 15:47:24 -0700

    Added selection sort algorithms as well as tests

--- sortingalgorithms/src/main/java/sortingalgorithms/Sort.java
+++ sortingalgorithms/src/main/java/sortingalgorithms/Sort.java
@@ -1,14 +1,24 @@
 package sortingalgorithms;
 
-import java.util.Arrays;
-
+/**
+ * Class Sort is used to sort an array of elements is ascending order.
+ *
+ * @param <T> The data type of the element.
+ */
 public class Sort<T extends Comparable<? super T>> {
-    private T[] elements;
+    private T[] elements;       // The elements to compare.
 
+    /**
+     * Constructs a Sort which can be used to sort arrays in ascending order.
+     * @param elements
+     */
     public Sort(T[] elements) {
         this.elements = elements;
     }
 
+    /**
+     * Uses the insertion sort algorithm to sort the array.
+     */
     public void insertionSort() {
         for (int i = 1; i < elements.length; i++) {
             T temp = elements[i];
@@ -18,6 +28,26 @@ public class Sort<T extends Comparable<? super T>> {
                     elements[j - 1] = temp;
                 }
             }
+        }
+    }
+
+    /**
+     * Uses the Selection Sort algorithm to sort the array.
+     */
+    public void selectionSort() {
+        for (int i = 0; i < elements.length; i++) {
+            T currentMinimum = elements[i];
+            int indexToRemember = i;
+            for (int j = i + 1; j < elements.length; j++) {
+                T currentItem = elements[j];
+                if (currentItem.compareTo(currentMinimum) < 0) {
+                    currentMinimum = currentItem;
+                    indexToRemember = j;
+                }
+            }
+            T temp = elements[i];
+            elements[i] = currentMinimum;
+            elements[indexToRemember] = temp;
         }
     }
 }
--- sortingalgorithms/src/test/java/sortingalgorithms/SortTests.java
+++ sortingalgorithms/src/test/java/sortingalgorithms/SortTests.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class SortTests {
 
+    /**----------------------------------- Insertion Sort Tests -------------------------------**/
     @Test
     void assert_InsertionSort_OneElement_works() {
         Integer[] array = new Integer[] {1};
@@ -38,6 +39,42 @@ public class SortTests {
         }
         Sort sort = new Sort(array);
         sort.insertionSort();
+        assertThat(array).isSorted();
+    }
+
+    /**----------------------------------- Selection Sort Tests -------------------------------**/
+    @Test
+    void assert_SelectionSort_OneElement_works() {
+        Integer[] array = new Integer[] {1};
+        Sort sort = new Sort(array);
+        sort.selectionSort();
+        assertThat(array).isSorted();
+    }
+
+    @Test
+    void assert_SelectionSort_TwoElements_works() {
+        Integer[] array = new Integer[] {2, 1};
+        Sort sort = new Sort(array);
+        sort.selectionSort();
+        assertThat(array).isSorted();
+    }
+
+    @Test
+    void assert_SelectionSort_ThreeElements_works() {
+        Integer[] array = new Integer[] {3, 2, 1};
+        Sort sort = new Sort(array);
+        sort.selectionSort();
+        assertThat(array).isSorted();
+    }
+
+    @Test
+    void assert_SelectionSort_20RandomElements_Orders_Correctly() {
+        Integer[] array = new Integer[20];
+        for (int i = 0; i < 20; i++) {
+            array[i] = Integer.valueOf((int) (Math.random() * 20 + 1));
+        }
+        Sort sort = new Sort(array);
+        sort.selectionSort();
         assertThat(array).isSorted();
     }
 }
